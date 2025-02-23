1d7dc95a71c8500e5eeacc2a06a9253ecf63b353
thibaultvanni <tibo.vanni@gmail.com>
2025-02-23 18:19:18 +0100

    version 1.0
    All the graphs can be done. Maybe need some adjustments.

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("hello world")
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+directory = "/Users/thibaultvanni/Documents/UCL/LELEC1360 - Télécommunications/Labo 1"
+
+print(os.listdir(directory))
+
+for filename in os.listdir(directory):
+    if filename.endswith(".csv"):
+        file = pd.read_csv(os.path.join(directory, filename), encoding='utf-8', skiprows=3)
+        file.columns = ['X', 'Y']
+
+        # Plotting
+        plt.plot(file['X'], file['Y'])
+        plt.title(f"Plot for {filename}")
+        plt.xlabel("X")
+        plt.ylabel("Y")
+        plt.tight_layout()  # Make sure the layout isn't clipped
+        plt.show()
+
+
