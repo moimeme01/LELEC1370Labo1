54b3b9fa0e9b452ffadbfbde45fbde5d579f0516
moimeme01 <tibo.vanni@gmail.com>
2025-02-24 11:22:10 +0100

    version 2.0
    All the graphs can be done. Either on mac and windows OS

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
-
-directory = "/Users/thibaultvanni/Documents/UCL/LELEC1360 - Télécommunications/Labo 1"
+"""
+Here is the windows version
+"""
+directory = "C:\\Users\\thibault\\Documents\\LabVIEW Projects\\Labo1_2025\\Documentation"
 
 print(os.listdir(directory))
 
@@ -20,3 +22,23 @@ for filename in os.listdir(directory):
         plt.show()
 
 
+"""
+Here is the MACos version
+
+directory = "thibault\Documents\LabVIEW Projects\Labo1_2025\Documentation"
+
+print(os.listdir(directory))
+
+for filename in os.listdir(directory):
+    if filename.endswith(".csv"):
+        file = pd.read_csv(os.path.join(directory, filename), encoding='utf-8', skiprows=3)
+        file.columns = ['X', 'Y']
+
+        # Plotting
+        plt.plot(file['X'], file['Y'])
+        plt.title(f"Plot for {filename}")
+        plt.xlabel("X")
+        plt.ylabel("Y")
+        plt.tight_layout()  # Make sure the layout isn't clipped
+        plt.show()
+"""
